dbfa140520a60b9d36c980fb5da4096c362e64bb
thomasgonzalezc <thomas.gonzalez3@misena.edu.co>
2021-09-13 11:44:30 -0500

    trabajamos lista, iamagenes y tablas

---
+++ HTML4/js/script.js
@@ -0,0 +1,7 @@
+ar username   = document.getElementById('username');
+const question = prompt("What's your username ?");
+if (question) {
+    username.innerText = "Welcome: " + question;
+} else {
+    username.innerText = "Welcome: Guess";
+}
